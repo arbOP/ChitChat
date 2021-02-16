f134b79e7fcc0f7ef5112009c8f255f73aeeba89
arbOP <newarbnor@hotmail.com>
2021-02-16 11:48:58 +0000

    added files and home page made

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+  root 'chats#index'
+
+  resources :messages do
+    resources :messages
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
